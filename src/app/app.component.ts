961baaa28d8b41218270828229037a0032520c38
irynavoronina <zefirka.n@gmail.com>
2018-08-06 18:35:11 +0200

    simple angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UsersService} from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [UsersService]
 })
 export class AppComponent {
-  title = 'app';
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {UserComponent} from './user/user.component';
+import {HttpClientModule} from '@angular/common/http';
+import { HoverDirective } from './hover.directive';
+import {FormsModule} from '@angular/forms';
+import { SearchPipe } from './search.pipe';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SetupPageComponent } from './setup-page/setup-page.component';
+import {RouterModule} from '@angular/router';
 
-
+const routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'setup', component: SetupPageComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    HoverDirective,
+    SearchPipe,
+    HomePageComponent,
+    SetupPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
